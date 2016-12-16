6ce7094c3f007db32c5f510805d69ae588368aec
Kunal Patel <kunal@Kunal.local>
2016-12-16 00:14:31 -0600

    learned php today and made a simple calculator

---
+++ php_files/calculator/calc_input.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>
+			Square me!
+		</title>
+	</head>
+	<style>
+		body{text-align:center}
+	</style>
+	<body>
+		<font size="16"> Welcome to my calculator!</font>
+		<form action="calc.php" method="post">
+			<br>
+			<input name="number1" placeholder="enter a number" type="text">
+			<input name="number2" placeholder="enter a number" type="text">
+			<br>
+			<br>
+			<input name="add" value='Add' type="submit">
+			<input name="sub" value='Subtract' type="submit">
+			<input name="div" value='Divide' type="submit">
+			<input name="mul" value='Multiply' type="submit">
+			<input name="squ" value='Square' type="submit">
+		</form>
+	</body>
+</html>
---
+++ php_files/helloworld.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>
+			Hello World!
+		</title>
+	</head>
+	<body>
+		<?php
+			$test = true;
+			if ($test == true){
+				echo "truth";
+			}else{
+				echo "php doesnt make sense";
+			}
+		?>
+
+	</body>
+</html>
